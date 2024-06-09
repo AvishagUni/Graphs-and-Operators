0261ed5ba17af6105b6af47a6b298400b81e7cb4
AvishagUni <AVISHAG.Minnes@msmail.ariel.ac.il>
2024-06-09 18:44:55 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -11,6 +11,7 @@ using namespace std;
 TEST_CASE("Test graph addition")
 {
     ariel::Graph g1;
+
     vector<vector<int>> graph = {
         {0, 1, 0},
         {1, 0, 1},
@@ -27,7 +28,7 @@ TEST_CASE("Test graph addition")
         {0, 2, 1},
         {2, 0, 3},
         {1, 3, 0}};
-    CHECK(g3.printGraph() == "[0, 2, 1]\n[2, 0, 3]\n[1, 3, 0]\n");
+    CHECK(g3.drawGraph() == "[0, 2, 1]\n[2, 0, 3]\n[1, 3, 0]\n");
 
     // Addition of two graphs with different dimensions
     ariel::Graph g5;
@@ -60,7 +61,7 @@ TEST_CASE("Test graph multiplication")
         {0, 0, 2},
         {1, 0, 1},
         {1, 0, 0}};
-    CHECK(g4.printGraph() == "[0, 0, 2]\n[1, 0, 1]\n[1, 0, 0]\n");
+    CHECK(g4.drawGraph() == "[0, 0, 2]\n[1, 0, 1]\n[1, 0, 0]\n");
 
     // Multiplication of two graphs with different dimensions
     ariel::Graph g5;
@@ -79,37 +80,7 @@ TEST_CASE("Test graph multiplication")
         {0, 2, 0},
         {2, 0, 2},
         {0, 2, 0}};
-    CHECK(g6.printGraph() == "[0, 2, 0]\n[2, 0, 2]\n[0, 2, 0]\n");
-}
-
-TEST_CASE("Invalid operations")
-{
-    ariel::Graph g1;
-    vector<vector<int>> graph = {
-        {0, 1, 0},
-        {1, 0, 1},
-        {0, 1, 0}};
-    g1.loadGraph(graph);
-    ariel::Graph g5;
-    vector<vector<int>> graph2 = {
-        {0, 1, 0, 0, 1},
-        {1, 0, 1, 0, 0},
-        {0, 1, 0, 1, 0},
-        {0, 0, 1, 0, 1},
-        {1, 0, 0, 1, 0}};
-    g5.loadGraph(graph2);
-    CHECK_THROWS(g5 * g1);
-
-    // Addition of two graphs with different dimensions
-    ariel::Graph g6;
-    vector<vector<int>> graph3 = {
-        {0, 1, 0, 0, 1},
-        {1, 0, 1, 0, 0},
-        {0, 1, 0, 1, 0},
-        {0, 0, 1, 0, 1},
-        {1, 0, 0, 1, 0}};
-    g6.loadGraph(graph3);
-    CHECK_THROWS(g1 + g6);
+    CHECK(g6.drawGraph() == "[0, 2, 0]\n[2, 0, 2]\n[0, 2, 0]\n");
 }
 
 TEST_CASE("Test unary plus operator")
@@ -121,7 +92,7 @@ TEST_CASE("Test unary plus operator")
         {2, 1, 0}};
     g1.loadGraph(graph);
     ariel::Graph g2 = +g1;
-    CHECK(g2.printGraph() == "[0, 2, 0]\n[-3, 0, 1]\n[2, 1, 0]\n");
+    CHECK(g2.drawGraph() == "[0, 2, 0]\n[-3, 0, 1]\n[2, 1, 0]\n");
 }
 
 TEST_CASE("Test unary minus operator")
@@ -133,7 +104,7 @@ TEST_CASE("Test unary minus operator")
         {10, 0, 0}};
     g1.loadGraph(graph);
     ariel::Graph g2 = -g1;
-    CHECK(g2.printGraph() == "[0, 6, 0]\n[-4, 0, 1]\n[-10, 0, 0]\n");
+    CHECK(g2.drawGraph() == "[0, 6, 0]\n[-4, 0, 1]\n[-10, 0, 0]\n");
 }
 
 TEST_CASE("Test graph equality")
@@ -156,7 +127,7 @@ TEST_CASE("Test graph equality")
     vector<vector<int>> graph3 = {
         {0, 1, 0},
         {1, 0, 1},
-        {0, 1, 1}};
+        {0, 2, 0}};
     g3.loadGraph(graph3);
     CHECK((g1 == g3) == false);
 }
@@ -179,33 +150,11 @@ TEST_CASE("Test graph inequality")
 
     ariel::Graph g3;
     vector<vector<int>> graph3 = {
-        {0, 1, 0},
-        {1, 0, 1},
-        {0, 1, 1}};
-    g3.loadGraph(graph3);
-    CHECK((g1 != g3) == true);
-}
-
-TEST_CASE("Test graph addition assignment")
-{
-    ariel::Graph g1;
-    vector<vector<int>> graph = {
-        {0, 1, 0},
+        {0, 1, 4},
         {1, 0, 1},
         {0, 1, 0}};
-    g1.loadGraph(graph);
-    ariel::Graph g2;
-    vector<vector<int>> weightedGraph = {
-        {0, 1, 1},
-        {1, 0, 2},
-        {1, 2, 0}};
-    g2.loadGraph(weightedGraph);
-    g1 += g2;
-    vector<vector<int>> expectedGraph = {
-        {0, 2, 1},
-        {2, 0, 3},
-        {1, 3, 0}};
-    CHECK(g1.printGraph() == "[0, 2, 1]\n[2, 0, 3]\n[1, 3, 0]\n");
+    g3.loadGraph(graph3);
+    CHECK((g1 != g3) == true);
 }
 
 TEST_CASE("Test += operator")
@@ -223,7 +172,7 @@ TEST_CASE("Test += operator")
         {1, 2, 0}};
     g2.loadGraph(weightedGraph);
     g1 += g2;
-    CHECK(g1.printGraph() == "[0, 2, 1]\n[2, 0, 3]\n[1, 3, 0]\n");
+    CHECK(g1.drawGraph() == "[0, 2, 1]\n[2, 0, 3]\n[1, 3, 0]\n");
 
     ariel::Graph g3;
     vector<vector<int>> grpah1 = {
@@ -241,18 +190,37 @@ TEST_CASE("Test += operator")
         {0, 0, 1, 0}};
     g4.loadGraph(grpah2);
     g3 += g4;
-    CHECK(g3.printGraph() == "[0, 13, 17, -2]\n[25, 0, 2, 0]\n[0, 1, 0, 1]\n[0, 0, 1, 0]\n");
+    CHECK(g3.drawGraph() == "[0, 13, 17, -2]\n[25, 0, 2, 0]\n[0, 1, 0, 1]\n[0, 0, 1, 0]\n");
+
+    ariel::Graph g5;
+    vector<vector<int>> graph3 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g5.loadGraph(graph3);
+    ariel::Graph g6;
+    vector<vector<int>> weightedGraph1 = {
+        {0, 1, 1},
+        {1, 0, 2},
+        {1, 2, 0}};
+    g6.loadGraph(weightedGraph1);
+    g5 += g6;
+    vector<vector<int>> expectedGraph1 = {
+        {0, 2, 1},
+        {2, 0, 3},
+        {1, 3, 0}};
+    CHECK(g5.drawGraph() == "[0, 2, 1]\n[2, 0, 3]\n[1, 3, 0]\n");
 
     // Addition of two graphs with different dimensions
-    ariel::Graph g6;
-    vector<vector<int>> graph3 = {
+    ariel::Graph g7;
+    vector<vector<int>> graph4 = {
         {0, 1, 0, 0, 1},
         {1, 0, 1, 0, 0},
         {0, 1, 0, 1, 0},
         {0, 0, 1, 0, 1},
         {1, 0, 0, 1, 0}};
-    g6.loadGraph(graph3);
-    CHECK_THROWS(g1 += g6);
+    g6.loadGraph(graph4);
+    CHECK_THROWS(g5 += g7);
 }
 
 TEST_CASE("Test - operator")
@@ -274,7 +242,7 @@ TEST_CASE("Test - operator")
         {0, 5, 0},
         {3, 0, 0},
         {10, -1, 0}};
-    CHECK(g3.printGraph() == "[0, 5, 0]\n[3, 0, 0]\n[10, -1, 0]\n");
+    CHECK(g3.drawGraph() == "[0, 5, 0]\n[3, 0, 0]\n[10, -1, 0]\n");
 
     // Subtraction of two graphs with different dimensions
     ariel::Graph g4;
@@ -307,7 +275,7 @@ TEST_CASE("Test -= operator")
         {0, 5, 0},
         {3, 0, 0},
         {10, -1, 0}};
-    CHECK(g1.printGraph() == "[0, 5, 0]\n[3, 0, 0]\n[10, -1, 0]\n");
+    CHECK(g1.drawGraph() == "[0, 5, 0]\n[3, 0, 0]\n[10, -1, 0]\n");
 
     ariel::Graph g3;
     vector<vector<int>> grpah1 = {
@@ -325,7 +293,7 @@ TEST_CASE("Test -= operator")
         {0, 0, 1, 0}};
     g4.loadGraph(grpah2);
     g3 -= g4;
-    CHECK(g3.printGraph() == "[0, 11, 17, -2]\n[23, 0, 0, 0]\n[0, -1, 0, -1]\n[0, 0, -1, 0]\n");
+    CHECK(g3.drawGraph() == "[0, 11, 17, -2]\n[23, 0, 0, 0]\n[0, -1, 0, -1]\n[0, 0, -1, 0]\n");
 
     // Subtraction of two graphs with different dimensions
     ariel::Graph g6;
@@ -358,7 +326,7 @@ TEST_CASE("Test * operator")
         {0, 0, 2},
         {1, 0, 1},
         {1, 0, 0}};
-    CHECK(g3.printGraph() == "[0, 0, 2]\n[1, 0, 1]\n[1, 0, 0]\n");
+    CHECK(g3.drawGraph() == "[0, 0, 2]\n[1, 0, 1]\n[1, 0, 0]\n");
 
     // Multiplication of two graphs with different dimensions
     ariel::Graph g4;
@@ -377,7 +345,7 @@ TEST_CASE("Test * operator")
         {0, 2, 0},
         {2, 0, 2},
         {0, 2, 0}};
-    CHECK(g5.printGraph() == "[0, 2, 0]\n[2, 0, 2]\n[0, 2, 0]\n");
+    CHECK(g5.drawGraph() == "[0, 2, 0]\n[2, 0, 2]\n[0, 2, 0]\n");
 }
 
 TEST_CASE("Test pre-increment operator")
@@ -389,7 +357,7 @@ TEST_CASE("Test pre-increment operator")
         {0, 1, 0}};
     g1.loadGraph(graph);
     ++g1;
-    CHECK(g1.printGraph() == "[0, 2, 1]\n[2, 0, 2]\n[1, 2, 0]\n");
+    CHECK(g1.drawGraph() == "[0, 2, 1]\n[2, 0, 2]\n[1, 2, 0]\n");
 
     ariel::Graph g2;
     vector<vector<int>> weightedGraph = {
@@ -399,7 +367,7 @@ TEST_CASE("Test pre-increment operator")
 
     g2.loadGraph(weightedGraph);
     ++g2;
-    CHECK(g2.printGraph() == "[0, 2, 2]\n[2, 0, 3]\n[2, 3, 0]\n");
+    CHECK(g2.drawGraph() == "[0, 2, 2]\n[2, 0, 3]\n[2, 3, 0]\n");
 
     // try to increment a graph with negative values
     ariel::Graph g3;
@@ -410,7 +378,7 @@ TEST_CASE("Test pre-increment operator")
         {230, -10, 0, 0}};
     g3.loadGraph(grpah1);
     ++g3;
-    CHECK(g3.printGraph() == "[0, 13, 18, -1]\n[25, 0, 2, 1]\n[1, -7, 0, 1]\n[231, -9, 1, 0]\n");
+    CHECK(g3.drawGraph() == "[0, 13, 18, -1]\n[25, 0, 2, 1]\n[1, -7, 0, 1]\n[231, -9, 1, 0]\n");
 }
 
 TEST_CASE("Test pre-decrement operator")
@@ -422,7 +390,7 @@ TEST_CASE("Test pre-decrement operator")
         {10, 0, 0}};
     g1.loadGraph(graph);
     --g1;
-    CHECK(g1.printGraph() == "[0, 5, -1]\n[3, 0, 0]\n[9, -1, 0]\n");
+    CHECK(g1.drawGraph() == "[0, 5, -1]\n[3, 0, 0]\n[9, -1, 0]\n");
 
     ariel::Graph g2;
     vector<vector<int>> weightedGraph = {
@@ -431,7 +399,7 @@ TEST_CASE("Test pre-decrement operator")
         {1, 2, 0}};
     g2.loadGraph(weightedGraph);
     --g2;
-    CHECK(g2.printGraph() == "[0, 0, 0]\n[0, 0, 1]\n[0, 1, 0]\n");
+    CHECK(g2.drawGraph() == "[0, 0, 0]\n[0, 0, 1]\n[0, 1, 0]\n");
 
     // try to decrement a graph with negative values
     ariel::Graph g3;
@@ -442,7 +410,7 @@ TEST_CASE("Test pre-decrement operator")
         {230, -10, 0, 0}};
     g3.loadGraph(grpah1);
     --g3;
-    CHECK(g3.printGraph() == "[0, 11, 16, -3]\n[23, 0, 0, -1]\n[-1, -9, 0, -1]\n[229, -11, -1, 0]\n");
+    CHECK(g3.drawGraph() == "[0, 11, 16, -3]\n[23, 0, 0, -1]\n[-1, -9, 0, -1]\n[229, -11, -1, 0]\n");
 }
 
 TEST_CASE("Test post-increment operator")
@@ -454,8 +422,8 @@ TEST_CASE("Test post-increment operator")
         {0, 1, 0}};
     g1.loadGraph(graph);
     ariel::Graph g2 = g1++;
-    CHECK(g1.printGraph() == "[0, 2, 1]\n[2, 0, 2]\n[1, 2, 0]\n");
-    CHECK(g2.printGraph() == "[0, 1, 0]\n[1, 0, 1]\n[0, 1, 0]\n");
+    CHECK(g1.drawGraph() == "[0, 2, 1]\n[2, 0, 2]\n[1, 2, 0]\n");
+    CHECK(g2.drawGraph() == "[0, 1, 0]\n[1, 0, 1]\n[0, 1, 0]\n");
 
     ariel::Graph g3;
     vector<vector<int>> weightedGraph = {
@@ -464,8 +432,8 @@ TEST_CASE("Test post-increment operator")
         {1, 2, 0}};
     g3.loadGraph(weightedGraph);
     ariel::Graph g4 = g3++;
-    CHECK(g3.printGraph() == "[0, 2, 2]\n[2, 0, 3]\n[2, 3, 0]\n");
-    CHECK(g4.printGraph() == "[0, 1, 1]\n[1, 0, 2]\n[1, 2, 0]\n");
+    CHECK(g3.drawGraph() == "[0, 2, 2]\n[2, 0, 3]\n[2, 3, 0]\n");
+    CHECK(g4.drawGraph() == "[0, 1, 1]\n[1, 0, 2]\n[1, 2, 0]\n");
 
     // try to increment a graph with negative values
     ariel::Graph g5;
@@ -476,8 +444,8 @@ TEST_CASE("Test post-increment operator")
         {230, -10, 0, 0}};
     g5.loadGraph(grpah1);
     ariel::Graph g6 = g5++;
-    CHECK(g5.printGraph() == "[0, 13, 18, -1]\n[25, 0, 2, 1]\n[1, -7, 0, 1]\n[231, -9, 1, 0]\n");
-    CHECK(g6.printGraph() == "[0, 12, 17, -2]\n[24, 0, 1, 0]\n[0, -8, 0, 0]\n[230, -10, 0, 0]\n");
+    CHECK(g5.drawGraph() == "[0, 13, 18, -1]\n[25, 0, 2, 1]\n[1, -7, 0, 1]\n[231, -9, 1, 0]\n");
+    CHECK(g6.drawGraph() == "[0, 12, 17, -2]\n[24, 0, 1, 0]\n[0, -8, 0, 0]\n[230, -10, 0, 0]\n");
 }
 
 TEST_CASE("Test post-decrement operator")
@@ -489,8 +457,8 @@ TEST_CASE("Test post-decrement operator")
         {10, 0, 0}};
     g1.loadGraph(graph);
     ariel::Graph g2 = g1--;
-    CHECK(g1.printGraph() == "[0, 5, -1]\n[3, 0, 0]\n[9, -1, 0]\n");
-    CHECK(g2.printGraph() == "[0, 6, 0]\n[4, 0, 1]\n[10, 0, 0]\n");
+    CHECK(g1.drawGraph() == "[0, 5, -1]\n[3, 0, 0]\n[9, -1, 0]\n");
+    CHECK(g2.drawGraph() == "[0, 6, 0]\n[4, 0, 1]\n[10, 0, 0]\n");
 
     ariel::Graph g3;
     vector<vector<int>> weightedGraph = {
@@ -499,8 +467,8 @@ TEST_CASE("Test post-decrement operator")
         {1, 2, 0}};
     g3.loadGraph(weightedGraph);
     ariel::Graph g4 = g3--;
-    CHECK(g3.printGraph() == "[0, 0, 0]\n[0, 0, 1]\n[0, 1, 0]\n");
-    CHECK(g4.printGraph() == "[0, 1, 1]\n[1, 0, 2]\n[1, 2, 0]\n");
+    CHECK(g3.drawGraph() == "[0, 0, 0]\n[0, 0, 1]\n[0, 1, 0]\n");
+    CHECK(g4.drawGraph() == "[0, 1, 1]\n[1, 0, 2]\n[1, 2, 0]\n");
 
     // try to decrement a graph with negative values
     ariel::Graph g5;
@@ -511,8 +479,8 @@ TEST_CASE("Test post-decrement operator")
         {230, -10, 0, 0}};
     g5.loadGraph(grpah1);
     ariel::Graph g6 = g5--;
-    CHECK(g5.printGraph() == "[0, 11, 16, -3]\n[23, 0, 0, -1]\n[-1, -9, 0, -1]\n[229, -11, -1, 0]\n");
-    CHECK(g6.printGraph() == "[0, 12, 17, -2]\n[24, 0, 1, 0]\n[0, -8, 0, 0]\n[230, -10, 0, 0]\n");
+    CHECK(g5.drawGraph() == "[0, 11, 16, -3]\n[23, 0, 0, -1]\n[-1, -9, 0, -1]\n[229, -11, -1, 0]\n");
+    CHECK(g6.drawGraph() == "[0, 12, 17, -2]\n[24, 0, 1, 0]\n[0, -8, 0, 0]\n[230, -10, 0, 0]\n");
 }
 
 TEST_CASE("Test / operator") {
@@ -527,7 +495,7 @@ TEST_CASE("Test / operator") {
         {0, 3, 0},
         {2, 0, 0},
         {5, 0, 0}};
-    CHECK(g2.printGraph() == "[0, 3, 0]\n[2, 0, 0]\n[5, 0, 0]\n");
+    CHECK(g2.drawGraph() == "[0, 3, 0]\n[2, 0, 0]\n[5, 0, 0]\n");
 
     ariel::Graph g3;
     vector<vector<int>> weightedGraph = {
@@ -540,7 +508,7 @@ TEST_CASE("Test / operator") {
         {0, 0, 0},
         {0, 0, 1},
         {0, 1, 0}};
-    CHECK(g4.printGraph() == "[0, 0, 0]\n[0, 0, 1]\n[0, 1, 0]\n");
+    CHECK(g4.drawGraph() == "[0, 0, 0]\n[0, 0, 1]\n[0, 1, 0]\n");
 
     // Division of graph with scalar 0
     CHECK_THROWS(g1 / 0);
@@ -558,7 +526,7 @@ TEST_CASE("Test /= operator") {
         {0, 3, 0},
         {2, 0, 0},
         {5, 0, 0}};
-    CHECK(g1.printGraph() == "[0, 3, 0]\n[2, 0, 0]\n[5, 0, 0]\n");
+    CHECK(g1.drawGraph() == "[0, 3, 0]\n[2, 0, 0]\n[5, 0, 0]\n");
 
     ariel::Graph g3;
     vector<vector<int>> weightedGraph = {
@@ -571,7 +539,7 @@ TEST_CASE("Test /= operator") {
         {0, 0, 0},
         {0, 0, 1},
         {0, 1, 0}};
-    CHECK(g3.printGraph() == "[0, 0, 0]\n[0, 0, 1]\n[0, 1, 0]\n");
+    CHECK(g3.drawGraph() == "[0, 0, 0]\n[0, 0, 1]\n[0, 1, 0]\n");
 
     // Division of graph with scalar 0
     CHECK_THROWS(g1 /= 0);
@@ -595,7 +563,7 @@ TEST_CASE("Test *= operator graphs") {
         {0, 0, 2},
         {1, 0, 1},
         {1, 0, 0}};
-    CHECK(g1.printGraph() == "[0, 0, 2]\n[1, 0, 1]\n[1, 0, 0]\n");
+    CHECK(g1.drawGraph() == "[0, 0, 2]\n[1, 0, 1]\n[1, 0, 0]\n");
 
     // Multiplication of two graphs with different dimensions
     ariel::Graph g3;
@@ -626,9 +594,9 @@ TEST_CASE("Test < operator") {
 
     ariel::Graph g3;
     vector<vector<int>> graph3 = {
-        {0, 1, 0},
+        {0, 1, 1},
         {1, 0, 1},
-        {0, 1, 1}};
+        {0, 1, 0}};
     g3.loadGraph(graph3);
     CHECK((g1 < g3) == true);
 }
@@ -652,7 +620,7 @@ TEST_CASE("Test > operator") {
     vector<vector<int>> graph3 = {
         {0, 1, 0},
         {1, 0, 1},
-        {0, 1, 1}};
+        {1, 1, 0}};
     g3.loadGraph(graph3);
     CHECK((g1 > g3) == false);
 }
@@ -674,9 +642,9 @@ TEST_CASE("Test <= operator") {
 
     ariel::Graph g3;
     vector<vector<int>> graph3 = {
-        {0, 1, 0},
+        {0, 1, 1},
         {1, 0, 1},
-        {0, 1, 1}};
+        {0, 1, 0}};
     g3.loadGraph(graph3);
     CHECK((g1 <= g3) == true);
 }
@@ -700,7 +668,7 @@ TEST_CASE("Test >= operator") {
     vector<vector<int>> graph3 = {
         {0, 1, 0},
         {1, 0, 1},
-        {0, 1, 1}};
+        {1, 1, 0}};
     g3.loadGraph(graph3);
     CHECK((g1 >= g3) == false);
 }
